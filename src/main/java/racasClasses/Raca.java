0bf98d4f2466ddacb7e19891f5d518c8eb729ff1
amarasantoyani <78428801+amarasantoyani@users.noreply.github.com>
2022-06-01 10:07:01 -0300

    Add files via upload

--- src/main/java/racasClasses/Raca.java
+++ src/main/java/racasClasses/Raca.java
@@ -4,4 +4,11 @@ public class Raca extends RacasClasses {
     public Raca(String nome) {
         super(nome);
     }
+    public static void ImprimeRacas(){
+        System.out.println("RACAS:");
+        System.out.println("1 - Elfo");
+        System.out.println("2 - Anao");
+        System.out.println("3 - Minotauro");
+        System.out.println("4 - Goblin");
+    }
 }
