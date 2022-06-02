e4d7419d64a073f34e47b1f672d7856c2bdd8e3c
User <User@DESKTOP-SR2I5M7>
2022-06-02 17:53:59 -0300

    Enumeração

--- src/main/java/racasClasses/Raca.java
+++ src/main/java/racasClasses/Raca.java
@@ -7,7 +7,7 @@ import atributos.Atributos;
 public class Raca extends RacasClasses {
     private boolean deslocamentoMaior, deslocamentoMenor; //atrib1, atrib2, atrib3, 4, 2, -2
     private String atributos;
-    enumRacas raca;
+    
     
             
     public Raca(String nome, boolean deslocamentoMaior, boolean deslocamentoMenor) {
