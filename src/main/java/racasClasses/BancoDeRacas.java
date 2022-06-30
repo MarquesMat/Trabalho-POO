52a77dcda673c7126edbdc4c6b44cbab311fb193
User <User@DESKTOP-SR2I5M7>
2022-06-30 17:06:11 -0300

    Metodo acrescentado

--- src/main/java/racasClasses/BancoDeRacas.java
+++ src/main/java/racasClasses/BancoDeRacas.java
@@ -26,10 +26,27 @@ public class BancoDeRacas {
         public static void ImprimeRacas(){
         System.out.println("RACAS:");
         
-        System.out.println("1 - Elfo(-2 CON, +4 INT, +2 DES)");
-        System.out.println("2 - Anao(+4 CON, +2 SAB, -2 DES)");
-        System.out.println("3 - Minotauro(+4 FOR, +2 CON, -2 SAB)");
-        System.out.println("4 - Goblin(+4 DES, +2 INT, -2 CAR)");
+        System.out.println("Elfo");
+        System.out.println("Anao");
+        System.out.println("Minotauro");
+        System.out.println("Goblin");
     }
-       
+       public static void EscolheRacas(){
+           System.out.println("1- Elfo");
+           System.out.println("Atributos: -2 em Constituição, +4 em inteligência, +2 em destreza ");
+           System.out.println("Poderes: ");//ainda a acrescentar
+           System.out.println(" ");
+           System.out.println("2- Anao");
+           System.out.println("Atributos: -2 em Destreza, +4 em Constituição,+2 em Sabedoria");
+           System.out.println("Poderes: ");//ainda a acrescentar
+           System.out.println(" ");
+           System.out.println("3- Minotauro");
+           System.out.println("Atributos: -2 em Sabedoria, +4 em Força,+2 em Constituição");
+           System.out.println("Poderes: ");//ainda a acrescentar
+           System.out.println(" ");
+           System.out.println("4- Anao");
+           System.out.println("Atributos: -2 em Carisma, +4 em Destreza,+2 em Inteligência");
+           System.out.println("Poderes: ");//ainda a acrescentar
+           System.out.println(" ");
+       }
 }
